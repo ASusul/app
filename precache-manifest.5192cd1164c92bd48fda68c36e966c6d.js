fe36990e5b257ed5fdae86677f41e00a99ecc10f
Adam Susuł <a.w.susul@gmail.com>
2020-09-03 19:23:00 +0200

    Updates

---
+++ precache-manifest.5192cd1164c92bd48fda68c36e966c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef66b2ed61666f10bb4699afac9b8718",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "3631929da1a511e520db",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "187ed1d0f3aa05864c11",
+    "url": "/app/static/js/2.5f31c038.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.5f31c038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3631929da1a511e520db",
+    "url": "/app/static/js/main.7cf3cf47.chunk.js"
+  },
+  {
+    "revision": "38449577f41ca32927a8",
+    "url": "/app/static/js/runtime-main.4310a04f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
